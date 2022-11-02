b59efce373cc0fb9fd840279b9ea344df48276b8
pavithran12345 <gunawathipavithran1234@gmail.com>
2022-11-02 15:20:56 +0530

    upload

---
+++ First.java
@@ -0,0 +1,53 @@
+package aravind;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*int no = 1; 
+		while(no<=10)
+		{
+		System.out.print(no+" "); 
+		no = no+2; 
+		}
+		/////////////////////////////////////////
+		//output:  
+
+	           //  1 3 5 7 9 
+		
+		////////////////////////////////////////*/
+		int no1 = 0; 
+		while(no1<=8)
+		{
+		System.out.print(no1+" "); 
+		no1 = no1+2; 
+		}
+		//////////////////////////////
+		//output:
+			
+			//0 2 4 6 8 
+			 
+		int no2 = 3; 
+		while(no2<=15)
+		{
+		System.out.print(no2+" "); 
+		no2 = no2+3; 
+		}
+		/////////////////////////
+		//output:
+			
+			//  3 6 9 12 15    
+		int no3 = 25; 
+		while(no3>=5)
+		{
+		System.out.print(no3+" "); 
+		no3 = no3-5; 
+		}
+		///////////////////////////////
+		//  output:
+		
+		//	25 20 15 10 5    */
+
+	}
+
+}
